99e4f5a3c81c85025f1cacb271c8b66a929180d8
Tsukasa5 <99931707+Tsukasa5@users.noreply.github.com>
2022-04-06 08:52:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,51 @@
 'use strict'
 
+let DefaultHTML;
+function DefaultSave() {
+  DefaultHTML = document.body.innerHTML;
+}
+function HTMLRestore() {
+  document.body.innerHTML = DefaultHTML;
+}
+
 const images = ["image/1.jpeg", "image/2.jpeg", "image/3.jpeg", "image/4.jpeg"];
 let point = 0;
 function randomIndication(){
-  document.getElementById("button0").style.display="none";
+  document.getElementById("button0").style.display='none';
   const imageArea = document.getElementById("imageArea");
   
   const imageNo = Math.floor(Math.random() * images.length);
   imageArea.src = images[imageNo];
-  // console.log(image)
-  // let i = 1;
-  // let image = images[imageNo][6]
-  // // function btnClick(element){
-  // const btnClick = (element) =>{
-  //   let pushId = element.target.id[6];
-  //   if (image === pushId){
-  //     point +=1;
-  //     i += 1;
-  //     console.log("i:" + i)
-  //     console.log("image:" + image);
-  //     console.log("pushID:" + pushId);
-  //   } else {
-  //     console.log("elseimage:" + image);
-  //     console.log("elsepushID:" + pushId);
-  //   }
-  // }
-  // button1.addEventListener('click', btnClick, false)//valueの値を使う
-  // button2.addEventListener('click', btnClick, false)
-  // button3.addEventListener('click', btnClick, false)
-  // button4.addEventListener('click', btnClick, false)
-}
-
-// function btnClick(e){
-  const btnClick = (element) =>{
-    const imageArea = document.getElementById("imageArea");
-    let image = imageArea.src[72]
+    const btnClick = (element) =>{
+    let image = images[imageNo][6]
     let pushId = element.target.id[6];
-    console.log(pushId)
     if (image === pushId){
       point +=1;
       console.log("image:" + image);
       console.log("pushID:" + pushId);
-      console.log("ポイント" + point)
     } else {
       console.log("elseimage:" + image);
       console.log("elsepushID:" + pushId);
+        // if (point < 15){
+        //   let comment = "視力が相当低下しているから、メガネを買いに行こう！";
+        // } else if (point < 25){
+        //   let comment = "";
+        // } else if (point < 35){
+        //   let comment = "";
+        // } else {
+        //   let comment = "";
+        // }
+      // const commentText = document.getElementsByClassName("side")[0];
+      // commentText.innerText = comment;
+      // HTMLRestore();
+      // point = 0;
     }
-    document.getElementById("button1").addEventListener('click', btnClick, false)
-    document.getElementById("button2").addEventListener('click', btnClick, false)
-    document.getElementById("button3").addEventListener('click', btnClick, false)
-    document.getElementById("button4").addEventListener('click', btnClick, false)
   }
-  
-
+  button1.addEventListener('click', btnClick, false)
+  button2.addEventListener('click', btnClick, false)
+  button3.addEventListener('click', btnClick, false)
+  button4.addEventListener('click', btnClick, false)
+}
 
 
 function reSize() {
@@ -65,9 +58,8 @@ function reSize() {
 }
 
 // window.onload = function () {
-//   let elmbtn = document.getElementById('button1');//正解を押したらに変更
+//   let elmbtn = document.getElementById('button1');
 //   elmbtn.onclick = function () {
 //     reSize();
 //   };
 // }
-
